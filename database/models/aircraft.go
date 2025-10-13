5e80479da9b1c811dfa95980b6c3a4c3ab7738cf
Lukman350 <nulhakiml725@gmail.com>
2025-10-13 14:52:53 +0700

    feat(aircraft): add aircraft auto migration when initialized

--- database/models/aircraft.go
+++ database/models/aircraft.go
@@ -1,27 +1,38 @@
 package models
 
-import "time"
+import (
+	"air-trail-backend/database"
+	"log"
+	"time"
+)
 
 // 'icao24','timestamp','acars','adsb','built','categoryDescription','country','engines','firstFlightDate','firstSeen','icaoAircraftClass','lineNumber','manufacturerIcao','manufacturerName','model','modes','nextReg','notes','operator','operatorCallsign','operatorIata','operatorIcao','owner','prevReg','regUntil','registered','registration','selCal','serialNumber','status','typecode','vdl'
 
 // '8a003c','2017-09-15 00:10:03',0,0,,'',Indonesia,'',,,L2J,'',AIRBUS,Airbus,A330 341,0,'','','',INDONESIA,'',GIA,Garuda Indonesia,'',,,PK-GPG,'','165','',A333,0
 
 type Aircraft struct {
-	Icao24           string `gorm:"primaryKey"`
-	Timestamp        *time.Time
-	Registration     string
-	Country          string
-	TypeCode         string
-	Owner            string
-	ManufacturerIcao string
-	ManufacturerName string
-	Model            string
-	Operator         string
-	OperatorIcao     string
-	OperatorCallsign string
-	OperatorIata     string
+	Icao24           string     `gorm:"primaryKey;size:12" json:"icao24"`
+	Timestamp        *time.Time `json:"timestamp"`
+	Registration     *string    `json:"registration"`
+	Country          *string    `json:"country"`
+	TypeCode         *string    `json:"type_code"`
+	Owner            *string    `json:"owner"`
+	ManufacturerIcao *string    `json:"manufacturer_icao"`
+	ManufacturerName *string    `json:"manufacturer_name"`
+	Model            *string    `json:"model"`
+	Operator         *string    `json:"operator"`
+	OperatorIcao     *string    `json:"operator_icao"`
+	OperatorCallsign *string    `json:"operator_callsign"`
+	OperatorIata     *string    `json:"operator_iata"`
 }
 
 func (Aircraft) TableName() string {
 	return "aircrafts_data"
+}
+
+func init() {
+	log.Println("[DATABASE] Running auto migration for table aircrafts_data ...")
+	if err := database.Pgsql.AutoMigrate(&Aircraft{}); err != nil {
+		log.Printf("[DATABASE] Auto migration failed for table aircrafts_data: %v\n", err)
+	}
 }
