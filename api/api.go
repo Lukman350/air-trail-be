e1bbb2c8ae85edb52de11bea0de306a550c1f095
Lukman350 <nulhakiml725@gmail.com>
2025-10-13 14:49:07 +0700

    feat(env): separate api services url

--- api/api.go
+++ api/api.go
@@ -6,5 +6,7 @@ import (
 
 var (
 	BASE_URL      = env.GetEnv("BASE_URL", "")
+	BASE_URL_BDG  = env.GetEnv("BASE_URL_BDG", "")
+	BASE_URL_NODE = env.GetEnv("BASE_URL_NODE", "")
 	JETPHOTOS_URL = env.GetEnv("JETPHOTOS_URL", "")
 )
